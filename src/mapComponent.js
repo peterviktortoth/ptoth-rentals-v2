874fe7433eb582114e3c50a917eac441728186db
peterviktortoth <152824719+peterviktortoth@users.noreply.github.com>
2023-12-26 08:40:38 -0800

    commit

--- src/mapComponent.js
+++ src/mapComponent.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
-import './styles.css'; // Include your styles
+import './styles.css';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoicGV0ZXJ2aWt0b3J0b3RoIiwiYSI6ImNscWN5bWM1ZzA3b3kyanBhMndyZW44eTMifQ.4le2l0XBKj7DKKYzu_LgyQ';
 
@@ -10,8 +10,41 @@ const MapComponent = ({ coordinates, radius }) => {
   const marker = useRef(null);
   const [isMapLoading, setIsMapLoading] = useState(true);
 
-  // Convert radius from miles to meters
-  const getRadiusInMeters = (miles) => miles * 1609.34;
+  // Convert radius from miles to kilometers
+  const getRadiusInKilometers = (miles) => miles * 1.60934;
+
+  const createCircleGeoJSON = (center, radiusInKm) => {
+    const points = 64;
+    const coords = [];
+
+    for (let i = 0; i < points; i++) {
+      const angle = (i * 360) / points;
+      const bearing = angle * Math.PI / 180;
+      coords.push(destinationPoint(center, radiusInKm, bearing));
+    }
+
+    return {
+      type: 'Feature',
+      geometry: {
+        type: 'Polygon',
+        coordinates: [coords]
+      }
+    };
+  };
+
+  const destinationPoint = (start, distance, bearing) => {
+    const R = 6371.01;
+    const lat1 = start[1] * Math.PI / 180;
+    const lon1 = start[0] * Math.PI / 180;
+    const dByR = distance / R;
+
+    const lat2 = Math.asin(Math.sin(lat1) * Math.cos(dByR) +
+      Math.cos(lat1) * Math.sin(dByR) * Math.cos(bearing));
+    const lon2 = lon1 + Math.atan2(Math.sin(bearing) * Math.sin(dByR) * Math.cos(lat1),
+      Math.cos(dByR) - Math.sin(lat1) * Math.sin(lat2));
+
+    return [lon2 * 180 / Math.PI, lat2 * 180 / Math.PI];
+  };
 
   useEffect(() => {
     if (!coordinates || !coordinates.latitude || !coordinates.longitude) return;
@@ -21,68 +54,97 @@ const MapComponent = ({ coordinates, radius }) => {
         container: mapContainerRef.current,
         style: 'mapbox://styles/mapbox/streets-v11',
         center: [coordinates.longitude, coordinates.latitude],
-        zoom: 15.2,
-        scrollZoom: false,
-        doubleClickZoom: false,
-        touchZoomRotate: false,
-        dragRotate: false,
-        keyboard: false
+        zoom: 13.2,
+        scrollZoom: true,
+        doubleClickZoom: true,
+        touchZoomRotate: true,
+        dragRotate: true,
+        keyboard: true,
       });
 
       marker.current = new mapboxgl.Marker()
         .setLngLat([coordinates.longitude, coordinates.latitude])
         .addTo(map.current);
 
-      // Add or update the circle layer when the style is fully loaded
-      map.current.on('styledata', () => {
-        if (map.current.getLayer('circle-radius')) {
-          map.current.setPaintProperty('circle-radius', 'circle-radius', getRadiusInMeters(radius));
-        } else {
-          map.current.addLayer({
-            id: 'circle-radius',
-            type: 'circle',
-            source: {
-              type: 'geojson',
-              data: {
-                type: 'Feature',
-                geometry: {
-                  type: 'Point',
-                  coordinates: [coordinates.longitude, coordinates.latitude]
-                }
-              }
-            },
-            paint: {
-              'circle-radius': getRadiusInMeters(radius),
-              'circle-color': 'blue',
-              'circle-opacity': 0.5
-            }
-          });
-        }
-      });
-
       map.current.on('load', () => {
         setIsMapLoading(false);
+        updateCircle();
       });
     }
   }, [coordinates]);
 
   useEffect(() => {
     if (map.current && map.current.isStyleLoaded()) {
-      const newZoomLevel = 15.2 - (radius - 0.1) * 6;
-      map.current.easeTo({
-        zoom: newZoomLevel,
-        duration: 1000, // Duration of the animation in milliseconds
-        essential: true
-      });
+      updateCircle();
     }
   }, [radius]);
+
+  const updateCircle = () => {
+    const radiusInKilometers = getRadiusInKilometers(radius);
+    const circleGeoJSON = createCircleGeoJSON([coordinates.longitude, coordinates.latitude], radiusInKilometers);
   
+    if (map.current.getSource('circle-source')) {
+      animateCircleTransition(circleGeoJSON, 500); // 1000 milliseconds for transition
+    } else {
+      map.current.addSource('circle-source', {
+        type: 'geojson',
+        data: circleGeoJSON
+      });
+  
+      map.current.addLayer({
+        id: 'circle-layer',
+        type: 'fill',
+        source: 'circle-source',
+        paint: {
+          'fill-color': 'blue',
+          'fill-opacity': 0.5
+        }
+      });
+    }
+  };
+  
+  const animateCircleTransition = (finalGeoJSON, duration) => {
+    const startTime = performance.now();
+    const initialGeoJSON = map.current.getSource('circle-source')._data;
+  
+    const animate = (currentTime) => {
+      const elapsedTime = currentTime - startTime;
+      const t = Math.min(1, elapsedTime / duration); // t ranges from 0 to 1
+  
+      const interpolatedGeoJSON = interpolateGeoJSON(initialGeoJSON, finalGeoJSON, t);
+      map.current.getSource('circle-source').setData(interpolatedGeoJSON);
+  
+      if (t < 1) {
+        requestAnimationFrame(animate);
+      }
+    };
+  
+    requestAnimationFrame(animate);
+  };
+  
+  const interpolateGeoJSON = (initialGeoJSON, finalGeoJSON, t) => {
+    const interpolated = {
+      ...initialGeoJSON,
+      geometry: {
+        ...initialGeoJSON.geometry,
+        coordinates: [initialGeoJSON.geometry.coordinates[0].map((coord, index) => {
+          const finalCoord = finalGeoJSON.geometry.coordinates[0][index];
+          return [
+            coord[0] + t * (finalCoord[0] - coord[0]),
+            coord[1] + t * (finalCoord[1] - coord[1])
+          ];
+        })]
+      }
+    };
+    return interpolated;
+    
+  };
 
   return (
     <div className="map-container">
       {isMapLoading && (
         <div className="spinner-container">
-          <div className="spinner-text">Loading map</div>
+          <div className="spinner-text">Loading map...</div>
           <div className="spinner"></div>
         </div>
       )}
